045ae072f70273d64a154a51c1e970dbe969e590
Rich Jones <rj-jones@live.com>
2026-09-15 13:26:10 +0100

    sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,36 @@
+import type { MetadataRoute } from "next";
+import { recipes } from "@/data/recipes";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://renalplan.com";
+
+  const staticPages: MetadataRoute.Sitemap = [
+    {
+      url: baseUrl,
+    },
+    {
+      url: `${baseUrl}/recipes`,
+    },
+    {
+      url: `${baseUrl}/planner`,
+    },
+    {
+      url: `${baseUrl}/nutrition`,
+    },
+    {
+      url: `${baseUrl}/requirements`,
+    },
+    {
+      url: `${baseUrl}/shopping`,
+    },
+    {
+      url: `${baseUrl}/about`,
+    },
+  ];
+
+  const recipePages: MetadataRoute.Sitemap = recipes.map((recipe) => ({
+    url: `${baseUrl}/recipes/${recipe.id}`,
+  }));
+
+  return [...staticPages, ...recipePages];
+}

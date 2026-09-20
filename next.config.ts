27c503a1ce07976e146ca53f2db3a7d553d45e01
Rich Jones <rj-jones@live.com>
2026-09-20 16:03:06 +0100

    correct next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.1.122"],
+
+  images: {
+    minimumCacheTTL: 60 * 60 * 24 * 30, // 30 days
+  },
 };
 
 export default nextConfig;
